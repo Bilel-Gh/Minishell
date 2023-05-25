87a753f14c8cbe46e2ee9128e3baa7f1ef85070c
Bilel GHANDRI <bghandri@e1r2p1.clusters.42paris.fr>
2023-05-25 04:37:21 +0200

    builtins v0 before parsing

---
+++ includes/minishell.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bghandri <bghandri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/25 04:32:12 by bghandri          #+#    #+#             */
+/*   Updated: 2023/05/25 04:32:22 by bghandri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <signal.h>
+
+extern char **environ;
+
+
--- src/main.c
+++ src/main.c
@@ -6,16 +6,12 @@
 /*   By: bghandri <bghandri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 20:12:28 by ncharii           #+#    #+#             */
-/*   Updated: 2023/05/24 22:36:08 by bghandri         ###   ########.fr       */
+/*   Updated: 2023/05/25 04:32:09 by bghandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <signal.h>
+#include "../includes/minishell.h"
+#include "builtins/builtins.c"
 
 // Gestionnaire de signal SIGINT
 void int_handler(int sig)
@@ -29,25 +25,38 @@ void int_handler(int sig)
     }
 }
 
+void minishell_loop(void)
+{
+    char *line;
+    while (1)
+    {
+        line = readline("minishell >");
+        if (line == NULL)
+            exit(0);
+        // args = split_line(line);
+        if (strcmp(line, "exit") == 0)
+            builtin_exit();
+        else if (strcmp(line, "pwd") == 0)
+            builtin_pwd(0);
+        else if (strcmp(line, "env") == 0)
+            builtin_env();
+        else
+            printf("Commande introuvable\n");
+        // Libérez la mémoire allouée par readline
+        free(line);
+    }
+}
+
 int main(void)
 {
+    char *line;
+    char **args;
     struct sigaction s_sigaction;
 
     s_sigaction.sa_handler = int_handler; // Nom de la fonction de gestionnaire
     sigaction(SIGINT, &s_sigaction, NULL); // Gestionnaire de signal SIGINT
 
-    char *line;
-    while (1)
-    {
-        line = readline("minishell >:");
-        if (line == NULL)
-            exit(0);
-
-        // Traitez la ligne de commande ici (par exemple, exécutez des commandes)
-		//printf("%s\n", line);
-        // Libérez la mémoire allouée par readline
-        free(line);
-    }
+    minishell_loop();
     return 0;
 }
 
