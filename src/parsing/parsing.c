fb51d88c15ddb11302e28042293c2840d647778d
Nassim home <chariinassim@gmail.com>
2023-05-28 10:24:38 +0200

    parsing en cour...

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -1,6 +1,6 @@
 #include "../../includes/minishell.h"
 
-int ft_parsing(int *type_token, int nb_token)
+bool error_grammaticale(int *type_token, int nb_token)
 {
     int i;
     i = 0;
@@ -8,7 +8,7 @@ int ft_parsing(int *type_token, int nb_token)
     if (nb_token == 1)
     {
         if ((type_token[i] != 0 ) && (type_token[i] != 1))
-            return(1);
+            return(true);
     }
     while (i < nb_token - 1)
     {
@@ -16,7 +16,7 @@ int ft_parsing(int *type_token, int nb_token)
         if (i == 0)
         {
             if ((type_token[i] != 0 ) && (type_token[i] != 1))
-                return (1);
+                return (true);
        }
         if (i != 0)
         {
@@ -25,15 +25,67 @@ int ft_parsing(int *type_token, int nb_token)
             {
               //  printf("i parsing.c = %d\n",i); 
                 if (type_token[i + 1] != 4 && type_token[i + 1] != 0 && type_token[i + 1] != 1)
-                    return (2);
+                    return (true);
             }
+            //!!!!!!! ATTENTION ls | > outfile !!!!!!!!
             if (type_token[i] == 4 && type_token[i + 1] != 0)
-                return (3);
+                return (true);
             if (type_token[i] == 1 && type_token[i + 1] != 0)
-                return (4);
+                return (true);
         }
         i++;
     }
  //   printf("*********ft_parsing***************\n");
+    return (false);
+}
+
+bool    redirection_error_size(char *redirection)
+{
+        int i;
+        
+        i = 0;
+        while (redirection[i])
+            i++;
+        if (i > 2)
+            return(true);
+        return (false);
+}
+   
+
+bool    error_size_and_spe_redirection(int *type_token, int nb_token, char **cont_token)
+{
+    int i;
+    i = 0;
+    printf("\n*********erreur parsing redirection ???***************\n");
+    while (i < nb_token)
+    {
+        if (type_token[i] == 1)
+        {
+            if(redirection_error_size(cont_token[i]))
+                return (true);
+            if(!only_type((cont_token[i])))
+                return (true);
+            give_sp_token((cont_token[i], type_token));
+        }
+        i++;
+    }
+
+}
+
+int ft_parsing(int *type_token, int nb_token, char **cont_token)
+{
+
+    if (error_grammaticale(type_token, nb_token))
+        return(1);
+    printf("\n********* no error grammaticale ***************\n");
+    if (error_size_and_spe_redirection(type_token, nb_token, cont_token))
+        return(1);
+    printf("\n********* no error redirection ***************\n");
+    if (error_quote(type_token, nb_token, cont_token))
+        return (1);
+    printf("\n*********    no error quote    ***************\n");
+
+
+
     return (0);
 }
